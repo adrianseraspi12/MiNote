0d31e3b7a990bb410bf0b533f468d84309c67e33
Adrian Seraspi <adrianseraspi12@gmail.com>
2018-09-29 18:23:28 +0800

    Some changes

---
+++ app/src/main/java/com/suzei/minote/utils/SquareView.java
@@ -0,0 +1,30 @@
+package com.suzei.minote.utils;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+
+public class SquareView extends AppCompatButton {
+
+    public SquareView(Context context) {
+        super(context);
+    }
+
+    public SquareView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = getMeasuredWidth();
+        setMeasuredDimension(width, width);
+    }
+
+}
