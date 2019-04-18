a67307a2cd7adf1e03faf002327191e6c8a4ddeb
Adrian Seraspi <adrianseraspi12@gmail.com>
2019-04-18 18:21:54 +0800

    Added Application Class for initializing AndroidThreeTenABP

---
+++ app/src/main/java/com/suzei/minote/MiNoteApp.kt
@@ -0,0 +1,13 @@
+package com.suzei.minote
+
+import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
+
+class MiNoteApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        AndroidThreeTen.init(this)
+    }
+
+}
