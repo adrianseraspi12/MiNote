c4c179112daa218eab4aaea3883457dc3b77f2de
aj <adrianseraspi12@gmail.com>
2021-01-16 18:35:12 +0800

    - Add custom toast message

--- app/src/main/java/com/suzei/minote/ui/list/ListActivity.kt
+++ app/src/main/java/com/suzei/minote/ui/list/ListActivity.kt
@@ -1,8 +1,13 @@
 package com.suzei.minote.ui.list
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.content.Intent
 import android.content.res.ColorStateList
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
@@ -19,6 +24,7 @@ import com.suzei.minote.ui.list.todo.ListTodoPresenter
 import com.suzei.minote.utils.dialogs.SelectNoteDialog
 import kotlinx.android.synthetic.main.activity_list.*
 import kotlinx.android.synthetic.main.custom_bottom_navigation.*
+import kotlinx.android.synthetic.main.toast_undo_delete.*
 
 class ListActivity : AppCompatActivity() {
 
@@ -35,6 +41,32 @@ class ListActivity : AppCompatActivity() {
         setupSelectNoteDialog()
         setupCustomBottomNavigation()
         setupFabClick()
+    }
+
+    fun showToastUndo(message: String, onUndoClick: (() -> Unit)) {
+        toast_undo_delete_root.alpha = 0f
+        toast_undo_delete_root.visibility = View.VISIBLE
+        toast_undo_delete_root
+                .animate()
+                .alpha(1f)
+                .duration = 300
+        toast_undo_delete_tv_title.text = message
+        toast_delete_btn_undo.setOnClickListener {
+            onUndoClick.invoke()
+        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            toast_undo_delete_root
+                    .animate()
+                    .alpha(0f)
+                    .setDuration(300)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            super.onAnimationEnd(animation)
+                            toast_undo_delete_root.visibility = View.GONE
+                            toast_undo_delete_root.animate().setListener(null)
+                        }
+                    })
+        }, 4000)
     }
 
     private fun setupSelectNoteDialog() {
