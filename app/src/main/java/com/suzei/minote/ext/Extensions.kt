a4598c72c3465c2792ef7e7c7a8acb5e16c38c1d
aj <adrianseraspi12@gmail.com>
2021-01-31 14:43:47 +0800

    - Added hide keyboard and scroll to bottom nested scrollview

--- app/src/main/java/com/suzei/minote/ext/Extensions.kt
+++ app/src/main/java/com/suzei/minote/ext/Extensions.kt
@@ -1,9 +1,13 @@
 package com.suzei.minote.ext
 
+import android.content.Context
 import android.content.res.Resources
 import android.graphics.Color
 import android.util.TypedValue
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import com.flask.colorpicker.ColorPickerView
 import com.flask.colorpicker.builder.ColorPickerDialogBuilder
@@ -45,9 +49,20 @@ fun Fragment.showColorWheel(title: String,
 }
 
 fun Int.setAlpha(alpha: Float): Int {
-    val alpha = (Color.alpha(this) * alpha).roundToInt()
+    val opacity = (Color.alpha(this) * alpha).roundToInt()
     val red = Color.red(this)
     val green = Color.green(this)
     val blue = Color.blue(this)
-    return Color.argb(alpha, red, green, blue)
+    return Color.argb(opacity, red, green, blue)
+}
+
+fun Fragment.hideKeyboard() {
+    val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+    imm?.hideSoftInputFromWindow(this.view?.windowToken, 0)
+}
+
+fun NestedScrollView.scrollToBottom() {
+    this.post {
+        this.fullScroll(View.FOCUS_DOWN)
+    }
 }
