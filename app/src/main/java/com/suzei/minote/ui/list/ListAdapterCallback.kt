4941e7345a00b71a2ea13ffb210566ae7b3f2045
aj <adrianseraspi12@gmail.com>
2021-01-31 18:30:01 +0800

    - Changed to optional function

--- app/src/main/java/com/suzei/minote/ui/list/ListAdapterCallback.kt
+++ app/src/main/java/com/suzei/minote/ui/list/ListAdapterCallback.kt
@@ -4,7 +4,7 @@ import com.suzei.minote.data.entity.Notes
 
 interface ListAdapterCallback {
 
-    fun onNotePasswordClick(note: Notes)
+    fun onNotePasswordClick(note: Notes) {}
 
     fun onNoteClick(itemId: String)
 
