dddd5857e2d95b0d1fd9468937b7fb41f7941bb3
aj <adrianseraspi12@gmail.com>
2021-02-14 20:47:48 +0800

    - Changed todo repository to interface

--- app/src/main/java/com/suzei/minote/ui/list/todo/ListTodoPresenter.kt
+++ app/src/main/java/com/suzei/minote/ui/list/todo/ListTodoPresenter.kt
@@ -2,12 +2,11 @@ package com.suzei.minote.ui.list.todo
 
 import com.suzei.minote.data.entity.Todo
 import com.suzei.minote.data.repository.Repository
-import com.suzei.minote.data.repository.TodoRepository
 import com.suzei.minote.ui.list.ListContract
 import com.suzei.minote.utils.LogMe
 
 class ListTodoPresenter(
-        private val repository: TodoRepository,
+        private val repository: Repository<Todo>,
         private val mView: ListContract.View<Todo>) :
         ListContract.Presenter<Todo> {
 
