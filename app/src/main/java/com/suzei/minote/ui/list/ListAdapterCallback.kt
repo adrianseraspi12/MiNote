ae52bd88876158399e2603cf746cedb7abd233d0
AJ Seraspi <adrianseraspi12@gmail.com>
2021-04-10 17:49:31 +0800

    Implemented new delete functionality

--- app/src/main/java/com/suzei/minote/ui/list/ListAdapterCallback.kt
+++ app/src/main/java/com/suzei/minote/ui/list/ListAdapterCallback.kt
@@ -2,10 +2,15 @@ package com.suzei.minote.ui.list
 
 import com.suzei.minote.data.entity.Notes
 
-interface ListAdapterCallback {
+interface ListAdapterCallback<T> {
 
     fun onNotePasswordClick(note: Notes) {}
 
     fun onNoteClick(itemId: String)
 
+    fun onNoteDeleted()
+
+    fun scrollTo(position: Int)
+
+    fun forceDelete(data: T)
 }
