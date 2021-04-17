d47c91323b08f6251a6f2e668700fbf36a8a3c08
AJ Seraspi <adrianseraspi12@gmail.com>
2021-04-17 19:41:54 +0800

    Added settings contains auto save

--- app/src/main/java/com/suzei/minote/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/suzei/minote/ui/settings/SettingsActivity.kt
@@ -1,18 +1,29 @@
 package com.suzei.minote.ui.settings
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.suzei.minote.R
 
 class SettingsActivity : AppCompatActivity() {
+
+    private val fragment = SettingsFragment.newInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.base_activity)
+        initPresenter()
         setupFragment()
     }
 
+    private fun initPresenter() {
+        SettingsPresenter(
+                fragment,
+                getPreferences(Context.MODE_PRIVATE)
+        )
+    }
+
     private fun setupFragment() {
-        val fragment = SettingsFragment.newInstance()
         val fm = supportFragmentManager
         fm.beginTransaction().replace(R.id.base_container, fragment).commit()
     }
