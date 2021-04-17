3855cf0c883f3b34e0cf1c60860340a88da3abd2
AJ Seraspi <adrianseraspi12@gmail.com>
2021-04-17 19:41:54 +0800

    Added settings contains auto save

--- app/src/main/java/com/suzei/minote/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/suzei/minote/ui/settings/SettingsFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.suzei.minote.databinding.FragmentSettingsBinding
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : Fragment(), SettingsContract.View {
+
+    private var _presenter: SettingsContract.Presenter? = null
+    private val presenter get() = _presenter!!
 
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
@@ -24,12 +28,33 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        presenter.setup()
         setupBackButton()
+        setupSwitch()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        _presenter = null
+    }
+
+    override fun setPresenter(presenter: SettingsContract.Presenter) {
+        this._presenter = presenter
+    }
+
+    override fun setDetails(isCheck: Boolean) {
+        binding.settingsSwitchAutoSave.isChecked = isCheck
+    }
+
+    override fun showToastMessage(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setupSwitch() {
+        binding.settingsSwitchAutoSave.setOnCheckedChangeListener { _, isCheck ->
+            presenter.setAutoSave(isCheck)
+        }
     }
 
     private fun setupBackButton() {
